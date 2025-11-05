d328977291dd075ab396bf9c866393756a5979c1
Silverclean091 <wjdghkdidi@naver.com>
2025-11-05 11:17:35 +0900

    Silver IV #2776: 암기왕

--- Silver/Silver IV/2776.py
+++ Silver/Silver IV/2776.py
@@ -1,19 +1,20 @@
 import sys
+input = sys.stdin.readline
 
-# 테스트케이스 입력
-t = int(sys.stdin.readline())
+# 테스트 케이스 개수 입력
+t = int(input())
 
 for i in range(t):
     note1_list = []
     note2_list = []
     
     # 수첩1의 정수의 개수 & 정수 입력
-    note1 = int(sys.stdin.readline())
-    note1_list = list(map(int, sys.stdin.readline().split()))
+    note1 = int(input())
+    note1_list = list(map(int, input().split()))
     
     # 수첩2의 정수의 개수 & 정수 입력
-    note2 = int(sys.stdin.readline())
-    note2_list = list(map(int, sys.stdin.readline().split()))
+    note2 = int(input())
+    note2_list = list(map(int, input().split()))
     
     # 수첩2의 내용이 수첩1에 있는지 확인
     for l in note2_list:
