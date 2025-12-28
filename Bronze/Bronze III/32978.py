2b6f851dc5be369e78c82dea9ac6379f07d7264d
Silverclean091 <wjdghkdidi@naver.com>
2025-12-29 00:16:40 +0900

    Bronze III #32978: 아 맞다 마늘

--- Bronze/Bronze III/32978.py
+++ Bronze/Bronze III/32978.py
@@ -1,5 +1,9 @@
 import sys
 input = sys.stdin.readline
 
-N = int(input())  #  요리 재료의 종류 개수 N
-ingredient = list(map(input.split()))
+N = int(input())   # 요리 재료의 개수 N 입력
+ingredient = set(input().split())   # 요리에 필요한 재료 집합
+hyeonbin_ing = set(input().split()) # 현빈이가 사용한 재료 집합
+
+for x in ingredient - hyeonbin_ing:  # 두 재료의 차집합
+    print(x)
